9befd5d82793f9be47865d0a8242dacb5d2e0d4a
linton-dawson <pushkarkukde@gmail.com>
2018-11-20 23:51:30 +0530

    Adding shapes and making minor changes

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include"svg.h"
 
 void drawrectangle(void);
+void drawallshapes(void);
+void customalientext(void);
 
 int main()
 {
@@ -12,7 +14,8 @@ int main()
 puts("Code in C starts - SVG\n--------------\n");
 
 drawrectangle();
-
+drawallshapes();
+customalientext();
 return EXIT_SUCCESS;
 
 }
@@ -28,6 +31,7 @@ void drawrectangle(void)
     }
     else
     {
+	svg_print(psvg);
         svg_rectangle(psvg, 512, 512, 0, 0, "white", "white", 0, 0, 0);
 
         svg_rectangle(psvg, 384, 384, 64, 64, "#00FF00", "#000000", 4, 0, 0);
@@ -40,3 +44,75 @@ void drawrectangle(void)
         svg_free(psvg);
     }
 } 
+
+void drawallshapes(void)
+{
+    svg* psvg;
+    psvg = svg_create(192, 320);
+
+    if(psvg == NULL)
+    {
+        puts("psvg is NULL");
+    }
+    else
+    {
+        svg_fill(psvg, "#DADAFF");
+
+        svg_text(psvg, 32, 32, "sans-serif", 16, "#000000", "#000000", "drawallshapes");
+        svg_circle(psvg, "#000080", 4, "#0000FF", 32, 64, 96);
+        svg_ellipse(psvg, 64, 160, 32, 16, "#FF0000", "#800000", 4);
+
+        svg_line(psvg, "#000000", 2, 32, 192, 160, 192);
+
+        svg_rectangle(psvg, 64, 64, 32, 224, "#00FF00", "#008000", 4, 4, 4);
+
+        svg_finalize(psvg);
+        svg_print(psvg);
+        svg_save(psvg, "allshapes.svg");
+        svg_free(psvg);
+    }
+}
+
+void customalientext(void)
+{
+    svg* psvg;
+    psvg = svg_create(512, 768);
+
+    if(psvg == NULL)
+    {
+        puts("psvg is NULL");
+    }
+    else
+    {
+        svg_fill(psvg, "#000010");
+
+        srand(time(NULL));
+        int x, y;
+        for(int s = 0; s <= 512; s++)
+        {
+            x = (rand() % 512);
+            y = (rand() % 768);
+
+            svg_rectangle(psvg, 1, 1, x, y, "white", "white", 0, 0, 0);
+        }
+
+        svg_text(psvg, 96, 712, "sans-serif", 32, "#FFFFFF", "#FFFFFF", "Custom Alien Text");
+
+        svg_circle(psvg, "silver", 2, "rgba(0,0,0,0)", 28, 256, 384);
+
+        svg_ellipse(psvg, 256, 374, 8, 14, "#808080", "#808080", 0);
+        svg_ellipse(psvg, 252, 372, 3, 2, "#000000", "#000000", 0);
+        svg_ellipse(psvg, 260, 372, 3, 2, "#000000", "#000000", 0);
+        svg_rectangle(psvg, 1, 1, 251, 371, "white", "white", 0, 0, 0);
+        svg_rectangle(psvg, 1, 1, 259, 371, "white", "white", 0, 0, 0);
+        svg_line(psvg, "black", 1, 254, 378, 258, 378);
+
+        svg_line(psvg, "silver", 2, 234, 416, 226, 432);
+        svg_line(psvg, "silver", 2, 278, 416, 286, 432);
+        svg_ellipse(psvg, 256, 400, 64, 16, "silver", "silver", 4);
+
+        svg_finalize(psvg);
+        svg_save(psvg, "iwanttobelieve.svg");
+        svg_free(psvg);
+    }
+}
--- svg.c
+++ svg.c
@@ -140,3 +140,91 @@ free(psvg->svg);
 free(psvg);
 
 }
+
+void svg_fill(svg* psvg, char* Fill)
+{
+
+svg_rectangle(psvg, psvg->width, psvg->height, 0, 0, Fill, Fill, 0, 0, 0);
+
+}
+
+void svg_print(svg* psvg)
+{
+
+printf("%s\n",psvg->svg);
+printf("\t\t-------------------------EOF-------------------------\n");
+
+}
+
+void svg_line(svg* psvg, char* stroke, int strokewidth, int x1, int y1, int x2, int y2)
+{
+    appendstr2svg(psvg, "    <line stroke='");
+    appendstr2svg(psvg, stroke);
+    appendstr2svg(psvg, "' stroke-width='");
+    appendnum2svg(psvg, strokewidth);
+    appendstr2svg(psvg, "px' y2='");
+    appendnum2svg(psvg, y2);
+    appendstr2svg(psvg, "' x2='");
+    appendnum2svg(psvg, x2);
+    appendstr2svg(psvg, "' y1='");
+    appendnum2svg(psvg, y1);
+    appendstr2svg(psvg, "' x1='");
+    appendnum2svg(psvg, x1);
+    appendstr2svg(psvg, "' />\n");
+}
+
+void svg_text(svg* psvg, int x, int y, char* fontfamily, int fontsize, char* fill, char* stroke, char* text)
+{
+    appendstr2svg(psvg, "    <text x='");
+    appendnum2svg(psvg, x);
+    appendstr2svg(psvg, "' y = '");
+    appendnum2svg(psvg, y);
+    appendstr2svg(psvg, "' font-family='");
+    appendstr2svg(psvg, fontfamily);
+    appendstr2svg(psvg, "' stroke='");
+    appendstr2svg(psvg, stroke);
+    appendstr2svg(psvg, "' fill='");
+    appendstr2svg(psvg, fill);
+    appendstr2svg(psvg, "' font-size='");
+    appendnum2svg(psvg, fontsize);
+    appendstr2svg(psvg, "px'>");
+    appendstr2svg(psvg, text);
+    appendstr2svg(psvg, "</text>\n");
+}
+
+void svg_circle(svg* psvg, char* stroke, int strokewidth, char* fill, int r, int cx, int cy)
+{
+    appendstr2svg(psvg, "    <circle stroke='");
+    appendstr2svg(psvg, stroke);
+    appendstr2svg(psvg, "' stroke-width='");
+    appendnum2svg(psvg, strokewidth);
+    appendstr2svg(psvg, "px' fill='");
+    appendstr2svg(psvg, fill);
+    appendstr2svg(psvg, "' r='");
+    appendnum2svg(psvg, r);
+    appendstr2svg(psvg, "' cy='");
+    appendnum2svg(psvg, cy);
+    appendstr2svg(psvg, "' cx='");
+    appendnum2svg(psvg, cx);
+    appendstr2svg(psvg, "' />\n");
+}
+
+void svg_ellipse(svg* psvg, int cx, int cy, int rx, int ry, char* fill, char* stroke, int strokewidth)
+{
+    appendstr2svg(psvg, "    <ellipse cx='");
+    appendnum2svg(psvg, cx);
+    appendstr2svg(psvg, "' cy='");
+    appendnum2svg(psvg, cy);
+    appendstr2svg(psvg, "' rx='");
+    appendnum2svg(psvg, rx);
+    appendstr2svg(psvg, "' ry='");
+    appendnum2svg(psvg, ry);
+    appendstr2svg(psvg, "' fill='");
+    appendstr2svg(psvg, fill);
+    appendstr2svg(psvg, "' stroke='");
+    appendstr2svg(psvg, stroke);
+    appendstr2svg(psvg, "' stroke-width='");
+    appendnum2svg(psvg, strokewidth);
+    appendstr2svg(psvg, "' />\n");
+}
+
